44c416bea5e5d9c9ab8fd54ebc3a6fa7cdff87ea
Joshua7792 <j.santiago39@hotmail.com>
2023-09-06 12:15:50 -0400

    fixed main

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 
 char **tokens = NULL;
 
+/**
+ * main - Monty bytecode interpreter.
+ * @ac: Argument count.
+ * @av: Argument vector.
+ *
+ * Description: This program reads and interprets Monty bytecode files
+ * to perform various stack operations.
+ *
+ * Return: 0 on success, EXIT_FAILURE on failure.
+ */
 int main(int ac, char **av)
 {
 	int input_verification, line_number = 0;
@@ -11,16 +21,9 @@ int main(int ac, char **av)
 	void (*valid_fun)(stack_t **, unsigned int, char *, FILE *) = NULL;
 	stack_t *stack = NULL;
 
-	if (ac != 2)
+	if (ac != 2 || (fd = fopen(av[1], "r")) == NULL)
 	{
 		dprintf(STDERR_FILENO, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	fd = fopen(av[1], "r");
-	if (fd == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -30,20 +33,14 @@ int main(int ac, char **av)
 		if (strcmp(cmd, "\n") == 0 || *cmd == '#')
 			continue;
 
-		tokens = tokenization(cmd, " \n");
-		if (tokens == NULL)
-		{
-			free(tokens);
+		if ((tokens = tokenization(cmd, " \n")) == NULL)
 			continue;
-		}
 
 		valid_fun = get_op_func(tokens[0]);
 		valid_fun(&stack, line_number, cmd, fd);
 
 		buffer = 0;
 		reset_inside(cmd, tokens);
-		cmd = NULL;
-		tokens = NULL;
 	}
 
 	free(cmd);
