ee343dbb53df39df658541b166bfba5cfac439b4
Joshua7792 <j.santiago39@hotmail.com>
2023-09-17 16:19:09 -0400

    fixed pint function

--- function2.c
+++ function2.c
@@ -43,7 +43,7 @@ void add(stack_t **stack, unsigned int line_number)
 void pint(stack_t **stack, unsigned int line_number)
 {
 	/* Check if the stack is empty */
-	if (stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
