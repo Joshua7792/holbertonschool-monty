cbd48ba6a685ef8850b3545b9351f3cb083e62ae
Joshua7792 <j.santiago39@hotmail.com>
2023-09-18 15:26:16 -0400

    try to fix the last checker

--- functions.c
+++ functions.c
@@ -8,28 +8,38 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
+	char *arg = strtok(NULL, " \t\n$");
 	stack_t *new_node;
 
-	/* Allocate memory for a new stack node */
+	if (!arg || !*arg || (*arg != '-' && !isdigit(*arg)))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 1; arg[i]; i++)
+	{
+		if (!isdigit(arg[i]))
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/* Set the integer value in the new node */
-	new_node->n = line_number;
-
-	/* Initialize pointers for the new node */
+	new_node->n = atoi(arg);
 	new_node->prev = NULL;
 	new_node->next = *stack;
 
-	/* Update the previous pointer of the current top element */
-	if (*stack != NULL)
+	if (*stack)
 		(*stack)->prev = new_node;
 
-	/* Update the stack pointer to point to the new top element */
 	*stack = new_node;
 }
 
